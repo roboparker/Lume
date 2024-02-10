65db961bd1345e352b0cdc16760459d30034b0e6
roboparker <robo.parker@proton.mail>
2024-02-10 02:47:32 -0600

    /hash password in factory

--- api/src/Factory/UserFactory.php
+++ api/src/Factory/UserFactory.php
@@ -4,6 +4,7 @@ namespace App\Factory;
 
 use App\Entity\User;
 use App\Repository\UserRepository;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Zenstruck\Foundry\ModelFactory;
 use Zenstruck\Foundry\Proxy;
 use Zenstruck\Foundry\RepositoryProxy;
@@ -32,8 +33,9 @@ final class UserFactory extends ModelFactory
     /**
      * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
      */
-    public function __construct()
-    {
+    public function __construct(
+        private UserPasswordHasherInterface  $passwordHasher,
+    ) {
         parent::__construct();
     }
 
@@ -44,7 +46,7 @@ final class UserFactory extends ModelFactory
     {
         return [
             'email' => self::faker()->text(180),
-            'password' => self::faker()->text(),
+            'plainPassword' => self::faker()->password(),
             'roles' => [],
         ];
     }
@@ -55,7 +57,9 @@ final class UserFactory extends ModelFactory
     protected function initialize(): self
     {
         return $this
-            // ->afterInstantiate(function(User $user): void {})
+            ->afterInstantiate(function (User $user) {
+                $user->setPassword($this->passwordHasher->hashPassword($user, $user->getPlainPassword()));
+            })
         ;
     }
 
