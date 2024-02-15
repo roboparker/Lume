11d6024108d3f3f2d21af441c5af28a2015d19b1
roboparker <robo.parker@proton.mail>
2024-02-15 16:35:54 -0600

    composer require --dev orm-fixtures

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -16,4 +16,5 @@ return [
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
