c039dfe80946a0b95e391088383d1ad137fa0bbd
roboparker <robo.parker@proton.mail>
2024-02-16 09:39:35 -0600

    Create a new api base class that uses zenstruck browser

---
+++ api/tests/Api/ApiTestCase.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Tests\Api;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Zenstruck\Browser\HttpOptions;
+use Zenstruck\Browser\KernelBrowser;
+use Zenstruck\Browser\Test\HasBrowser;
+use Zenstruck\Foundry\Test\Factories;
+use Zenstruck\Foundry\Test\ResetDatabase;
+
+class ApiTestCase extends KernelTestCase
+{
+    use Factories;
+    use HasBrowser {
+        browser as baseKernelBrowser;
+    }
+    use ResetDatabase;
+
+    protected function browser(): KernelBrowser
+    {
+        return $this->baseKernelBrowser()
+            ->setDefaultHttpOptions(
+                HttpOptions::create()->withHeader('Accept', 'application/ld+json')
+            );
+    }
+}
